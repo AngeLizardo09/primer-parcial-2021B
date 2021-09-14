8a2dc8143c4facce9d3eb0bef6b98424c92c07a5
Ivan Israel Uresti Adame <ivan.uresti@encora.com>
2021-09-14 08:44:22 -0500

    Solución al examen

---
+++ src/edu/uaslp/examen/BookCollection.java
@@ -0,0 +1,36 @@
+package edu.uaslp.examen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookCollection {
+    private String name;
+    private List<Book> books;
+
+    public BookCollection(String name){
+        this.name = name;
+        books = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addBook(Book book){
+        books.add(book);
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public int getStars(){
+        int average = 0;
+
+        for(Book book: books){
+            average += book.getStars();
+        }
+
+        return average / books.size(); // División entera
+    }
+}
---
+++ src/edu/uaslp/examen/BookCollectionManager.java
@@ -0,0 +1,62 @@
+package edu.uaslp.examen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookCollectionManager {
+
+    private List<BookCollection> collections;
+
+    public BookCollectionManager() {
+        collections = new ArrayList<>();
+    }
+
+    public BookCollection createCollection(String name) {
+        BookCollection bookCollection = new BookCollection(name);
+
+        collections.add(bookCollection);
+
+        return bookCollection;
+    }
+
+    public BookCollection getCollectionByName(String name) {
+        for (BookCollection bookCollection : collections) {
+            if (bookCollection.getName().equals(name)) {
+                return bookCollection;
+            }
+        }
+
+        return null;
+    }
+
+
+    public void deleteCollectionByName(String name) {
+        for (BookCollection bookCollection : collections) {
+            if (bookCollection.getName().equals(name)) {
+                collections.remove(bookCollection);
+                return;
+            }
+        }
+    }
+
+    public void addBookToCollection(String collectionName, Book book){
+        BookCollection bookCollection = getCollectionByName(collectionName);
+
+        if(bookCollection == null){
+            bookCollection = createCollection(collectionName);
+        }
+
+        bookCollection.addBook(book);
+    }
+
+    public List<String> getCollectionNames(){
+        List<String> collectionNames = new ArrayList<>();
+
+        for(BookCollection bookCollection: collections){
+            collectionNames.add(bookCollection.getName());
+        }
+
+        return collectionNames;
+    }
+
+}
